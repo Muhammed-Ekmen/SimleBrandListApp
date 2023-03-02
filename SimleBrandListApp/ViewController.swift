8dcfb0bea2234a07aabc34d47e30b93c25f24481
Muhammed Semih <muhammedsemihekmen@hotmail.com>
2023-03-02 16:02:27 +0300

    ready to take notes

--- SimleBrandListApp/ViewController.swift
+++ SimleBrandListApp/ViewController.swift
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
+    @IBOutlet weak var helloLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
 
